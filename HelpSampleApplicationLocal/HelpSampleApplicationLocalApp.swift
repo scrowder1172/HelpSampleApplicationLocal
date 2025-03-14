a744254db021f4dcff601f073a50c9bda1412bb4
Scott Crowder <scrowder@gmail.com>
2025-03-14 11:03:14 -0500

    Initial Commit

---
+++ HelpSampleApplicationLocal/HelpSampleApplicationLocalApp.swift
@@ -0,0 +1,20 @@
+//
+// File: HelpSampleApplicationLocalApp.swift
+// Project: HelpSampleApplicationLocal
+// 
+// Created by SCOTT CROWDER on 3/14/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct HelpSampleApplicationLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
